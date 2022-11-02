79e1970f264ef72f7235c6551c79a8ae5ba2d7a4
Agustin Aguero <agustin.aguero@gmail.com>
2022-11-01 21:59:08 -0400

    Final entrenamiento 1

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 /*
 */
+
+Route::apiResource('books', BookController::class);
